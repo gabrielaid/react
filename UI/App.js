b96c9048bee66380f255a1ef9dad7956d59761ad
gabrielaid <167202616+gabrielaid@users.noreply.github.com>
2024-10-30 14:03:14 +0200

    Add files via upload

---
+++ UI/App.js
@@ -0,0 +1,49 @@
+import React, {useState} from  'react';
+
+import NewExpense from '../NewExpense/NewExpense';
+import Expenses from '../components/Expenses/Expenses'; 
+
+
+  const dummy_expenses = [
+    {
+      id: 'e1',
+      title: 'Toilet Paper',
+      amount: 94.12,
+      date: new Date("2022-07-14"),
+    },
+    {id: 'e2', title: 'New TV', amount: 799.49, date: new Date("2023-01-12"),},
+    {
+      id: 'e3',
+      title: 'Car Insurance',
+      amount: 300.00,
+      date: new Date("2023-01-01"),
+    },
+    {
+      id: 'e4',
+      title: 'New Desk (Wooden)',
+      amount: 450,
+      date: new Date("2022-05-12"),
+    },
+  ];
+
+  const App=() => {
+
+    const [expenses, setExpenses] = useState(dummy_expenses);
+  
+  const addExpenseHandler = expense => {
+    setExpenses((preExpenses) => {
+      return [expense, ...preExpenses];
+    });
+    /*console.log('In App.js');
+    //console.log(expense);*/
+  };
+  return (
+    <div>
+      <NewExpense onAddExpense={addExpenseHandler}/>
+      <Expenses items={expenses} />
+      <p>Gabriela / VAMK / BIT</p>
+    </div>
+  );
+}
+
+export default App;
